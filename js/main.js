b20d6dd048062bf4f2eb6b35147322d384b9e31e
AdamReszka <adam.reszka85@gmail.com>
2017-05-03 18:44:24 +0200

    add paralax js file and parallax effect to main photo

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@ $(document).ready(initAll);
 
 function initAll() {
   setHeight();
+  startParallax();
 }
 
 function setHeight() {
   $('#sec1-landing').height($(window).innerHeight());
+}
+
+function startParallax() {
+  $('.parallax-window').parallax({imageSrc: '../images/sec1-main-photo.png'});
 }
